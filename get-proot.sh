279881356cb00ba026951bb1d26290bf4b560cc0
alufers <alufers@wp.pl>
2023-03-24 21:14:02 +0100

    Change proot to be a fork

--- get-proot.sh
+++ get-proot.sh
@@ -9,4 +9,4 @@ cd "$BUILD_DIR"
 
 if [ -d "proot-$PROOT_V" ] ; then exit 0 ; fi
 
-wget -O - "https://github.com/green-green-avk/proot/archive/v$PROOT_V.tar.gz" | tar -xzv
+wget -O - "https://github.com/alufers/proot/archive/refs/tags/$PROOT_V.tar.gz" | tar -xzv
